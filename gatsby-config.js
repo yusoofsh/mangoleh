57e2a7a4f8dba8d2b1e70ed9b16ab8730d603446
Yu Soof <fuzuyx@gmail.com>
2020-10-17 19:03:22 +0700

    build(gatsby): 🔨 Update image resolution.
    Signed-off-by: Yu Soof <fuzuyx@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Personal | Yu Soof',
-    description: `I'm a software engineer with experience building mobile application
-      for medical consultation, IoT emergency dispatcher
-      and real-time notification.`,
+    title: 'Yu Soof',
+    description: 'Na am gud.',
     siteUrl: 'https://yusoof.io', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/src/images/demo.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@ouysof',
   },
   plugins: [
